46c40963478b61251a3ed919e2f1fbf1054989e7
Vagelis Kormazos <kormazos.v@gmail.com>
2025-12-22 20:16:02 +0200

    Add  ViewModels

---
+++ ViewModels/BookIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryManagementSystem.ViewModels
+{
+	public class BookIndexViewModel
+	{
+		// Η λίστα των βιβλίων που θα εμφανίζεται
+		public IEnumerable<LibraryManagementSystem.Models.Book> Books { get; set; }
+
+		// Πεδία για τα φίλτρα αναζήτησης
+		public string? GenreFilter { get; set; }
+		public int? YearFilter { get; set; }
+
+		// Λίστα για το dropdown των ειδών (προαιρετικά)
+		public List<string>? Genres { get; set; }
+	}
+}
---
+++ ViewModels/ReviewCreateViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.ViewModels
+{
+	public class ReviewCreateViewModel
+	{
+		public int BookId { get; set; }
+		public string? BookTitle { get; set; }
+
+		[Required(ErrorMessage = "Content is required")]
+		[StringLength(500, MinimumLength = 10, ErrorMessage = "Content must be between 10 and 500 characters")]
+		public string Content { get; set; }
+
+		[Required]
+		[Range(1, 5, ErrorMessage = "Rating must be between 1 and 5")]
+		public int Rating { get; set; }
+	}
+}
